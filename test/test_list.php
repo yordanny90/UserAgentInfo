d41dd240a36da2fe92fe06c04349543036f78261
Yordanny Mejías <yordanny90@gmail.com>
2024-05-24 15:24:38 -0600

    Update test_list.php

--- test/test_list.php
+++ test/test_list.php
@@ -6,7 +6,11 @@ spl_autoload_register();
 if(php_sapi_name()!='cli') echo '<pre>';
 $list=fopen(__DIR__.'/useragent_list.txt', 'r');
 if(!$list) throw new Exception('missing test file');
+$t=microtime(true);
+$count=0;
 while(is_string($userAgent=fgets($list))){
+    ++$count;
     $ua=new UserAgentInfo($userAgent);
     echo "OS: ".implode(' ', $ua->getOS()??['N/R'])."\tBrowser: ".implode(' ', $ua->getBrowser()??['N/R'])."\n";
 }
+echo 'Items: '.$count.' Time: '.(microtime(true)-$t);
