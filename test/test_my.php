a74df40eaffb6c44ec4434ee9d3b635422138700
Yordanny Mejías <yordanny90@gmail.com>
2024-05-28 00:09:04 -0600

    Update test_my.php

--- test/test_my.php
+++ test/test_my.php
@@ -1,7 +1,5 @@
 <?php
-set_include_path(__DIR__.'/../src');
-spl_autoload_extensions('.php');
-spl_autoload_register();
+include '../src/UserAgentInfo.php';
 
 $my=UserAgentInfo::my();
 echo '<h1>'.$my->getUserAgent().'</h1><h2>OS: '.implode(' ', $my->getOS()??['N/R']).'</h2><h2>Browser: '.implode(' ', $my->getBrowser()??['N/R']).'</h2>';
