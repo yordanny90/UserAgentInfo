7085c82cad2bd826f77919f915a484ce673e86f9
Yordanny Mejías <yordanny90@gmail.com>
2024-12-10 23:43:25 -0600

    Update test_my.php

--- test/test_my.php
+++ test/test_my.php
@@ -2,4 +2,4 @@
 include '../src/UserAgentInfo.php';
 
 $my=UserAgentInfo::my();
-echo '<h1>'.$my->getUserAgent().'</h1><h2>OS: '.implode(' ', $my->getOS()??['N/R']).'</h2><h2>Browser: '.implode(' ', $my->getBrowser()??['N/R']).'</h2>';
+echo '<h1>'.UserAgentInfo::me().'</h1><h2>'.UserAgentInfo::explain_me()->text_D().'</h2>';
